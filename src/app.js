8cd827b87509c13dd2befd6eec6aefbc0d83e6b9
rafael <you@example.com>
2025-05-08 18:26:55 -0300

    create the routes

--- src/app.js
+++ src/app.js
@@ -2,4 +2,13 @@ const express = require('express');
 
 const app = express();
 
+app.get('/figher/:id', (req, res) => res.status(200).json({ message: "Olá Mundo!" }));
+
+app.post('/figher/', (req, res) => res.status(200).json({ message: "Olá Mundo!" }));
+
+app.put('/figher/:id', (req, res) => res.status(200).json({ message: "Olá Mundo!" }));
+
+app.delete('/figher/:id', (req, res) => res.status(200).json({ message: "Olá Mundo!" }));
+
+
 module.exports = app;
