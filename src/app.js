6a88f3a45868bd33695cebc0ebe2caeafabf3cf8
rafael <you@example.com>
2025-06-26 17:50:43 -0300

    add cors to app.js

--- src/app.js
+++ src/app.js
@@ -1,13 +1,15 @@
 const express = require('express');
+const cors = require('cors');
 const fighterRouter = require('./routes/fighterRouter');
 const rankingRouter = require('./routes/rankingRouter');
 const eventRouter = require('./routes/eventRouter');
 const fightRouter = require('./routes/fightRouter');
 
-
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
+
+app.use(cors());
 
 app.use('/fighter', fighterRouter);
 
